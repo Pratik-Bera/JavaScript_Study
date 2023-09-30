c02e8efc1355232b42b9871377409394262ff193
Pratik Bera <pratikbera.it@gmail.com>
2023-09-30 15:01:09 +0530

    new folder added

---
+++ 01Intro/02VariablesDeclaration.js
@@ -0,0 +1,21 @@
+var a1 = 45   //"It was use in early. but that is nor recomanded now. There fore we need to use let kw"
+console.log(a1)
+
+//recomanded way
+let a2 = "This is recomanded way."
+console.log(a2)
+
+// we also use const but there has a different purpose
+const a3 = "This is const"    // we need assign it now and we can't change the value of it.
+console.log(a3)
+
+//a3="How is your day?"     // that is gives an Error. beacause a3 is const.
+
+//Use Cases :
+let str = "I love to travel."
+console.log(str)
+console.log(typeof (str))
+str = 45                      // Js gives the permission to change the datatypes of variable.so you can store another data types is same variable.
+console.log(str)
+console.log(typeof (str))
+
---
+++ 01Intro/03DataTypes.js
@@ -0,0 +1,46 @@
+// there has 7 premitive data types and 3 object data types support in JavaScript 
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+console.log(length,",", weight)
+console.log(typeof(length))
+console.log(typeof(weight))
+
+// Strings:
+let color = "Pratik";
+let lastName = "Bera";
+console.log(color,",", lastName)
+console.log(typeof(color))
+console.log(typeof(lastName))
+
+
+// Booleans
+let x = true;
+let y = false;
+console.log(x,",", y)
+console.log(typeof(x))
+console.log(typeof(y))
+
+let unde
+console.log(typeof(unde))
+
+let null2=null;
+console.log(typeof(null2))
+
+
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+console.log(person)
+console.log(typeof(person))
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars)
+console.log(typeof(cars))
+
+// Date object:
+const date = new Date("2022-03-25");
+console.log(date)
+console.log(typeof(date))
