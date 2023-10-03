d84e51127cf54be929f0b8954cc14f11c8986ce6
Pratik Bera <pratikbera.it@gmail.com>
2023-10-03 19:30:37 +0530

    The Full Folder is completed

--- 05Array/03ArrayWithLoop.js
+++ 05Array/03ArrayWithLoop.js
@@ -1,12 +1,41 @@
 let arr=[45,89,12,76,10,5,36]
 
-for()
+//Displaying using besic for loop in js 
+console.log("Displaying using Besic Loop in js:");
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
 
 
 //For Each Loop
 
+console.log("Displaying using forEach Loop in js:");
 arr.forEach((element)=>{            //forEach method on an array to iterate over its elements and log each element to the console.    
     element+=1;
-    console.log(element)
+    console.log(element);
 })
+
+//For-in loop in Array
+console.log("Displaying using for-in Loop in js:");
+for(let i in arr){
+    console.log(arr[i]);
+    
+}
+
+//for-of loop in Array
+console.log("Display using for-of loop in array :");
+for(let i of arr){
+    console.log(i);  
+}
+
+//array from
+let ar=[4,8,20,45,2];
+let arr2=Array.from(ar);
+console.log(ar);
+console.log(arr2);
+
+let st="Pratik";
+ar=Array.from(st);
+console.log(st);
+console.log(ar);
 
